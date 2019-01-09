a979169a31b2911b6ab0bd93b7b6759cd157f7ce
hanna_nyman <hannanyman.sk@gmail.com>
2019-01-09 13:12:12 +0100

    Creates createBoard function, adds state to Minesweeper component

--- src/Minesweeper.js
+++ src/Minesweeper.js
@@ -2,6 +2,13 @@ import React, { Component } from 'react';
 import './Minesweeper.css';
 
 class Minesweeper extends Component {
+  state = {
+    rows: 10,
+    columns: 10,
+    flags: 10,
+    mines: 10
+  }
+
   render() {
     return (
       <div className="minesweeper">
--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 
 class Board extends Component {
+
+    createBoard = (props) => {
+        let board = []
+
+        for (let i = 0; i < props.rows; i++) {
+            
+        }
+    }
+
     render() {
         return (
             <div>
