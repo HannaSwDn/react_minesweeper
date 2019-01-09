bd074ecca1771d70947e35538ec97e23523e6c30
hanna_nyman <hannanyman.sk@gmail.com>
2019-01-09 13:06:05 +0100

    Adds folder structure for components, adds Board component

---
+++ src/Minesweeper.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './Minesweeper.css';
+
+class Minesweeper extends Component {
+  render() {
+    return (
+      <div className="minesweeper">
+        
+      </div>
+    );
+  }
+}
+
+export default Minesweeper;
